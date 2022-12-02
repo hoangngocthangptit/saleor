ee62d4a2b50b2374d7be872ecdd71de81919c320
Hoàng Ngọc Thắng <thanghn@neo.vn>
2022-12-02 14:01:58 +0700

    start

---
+++ saleor-dashboard/src/themeOverrides.ts
@@ -0,0 +1,25 @@
+import { Theme } from "@material-ui/core/styles";
+
+const breakpoints = ({
+  keys: ["xs", "sm", "md", "lg", "xl"],
+  values: { lg: 1680, md: 1280, sm: 600, xl: 1920, xs: 0 },
+} as unknown) as Theme["breakpoints"];
+
+const themeOverrides: Partial<Theme> = {
+  breakpoints,
+  overrides: {
+    MuiTableCell: {
+      body: {
+        paddingBottom: 8,
+        paddingTop: 8,
+      },
+      root: {
+        height: 56,
+        paddingBottom: 4,
+        paddingTop: 4,
+      },
+    },
+  },
+};
+
+export default themeOverrides;
